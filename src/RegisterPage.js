fb3c8a389ec4a6dfff74bd83df499ac254a0f427
dhanushkiren <dhanushrameshkvpt03@gmail.com>
2024-06-14 18:57:49 +0530

    reg change

--- src/RegisterPage.js
+++ src/RegisterPage.js
@@ -1,39 +1,42 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './RegisterPage.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import "./RegisterPage.css";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [error, setError] = useState('');
-  const [message, setMessage] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
 
   const navigate = useNavigate();
 
   const handleRegister = async () => {
     try {
-      setError('');
-      const response = await axios.post('https://refreshing-expression-production.up.railway.app/new', {
-       userName: username,
-        password,
-       emailId: email,
-       role:"USER"
-      });
+      setError("");
+      const response = await axios.post(
+        "https://refreshing-expression-production.up.railway.app/new",
+        {
+          userName: username,
+          password,
+          emailId: email,
+          role: "USER",
+        }
+      );
 
       if (response.status === 200) {
-        setMessage('Registration Successful');
-        setUsername('');
-        setPassword('');
-        setEmail('');
-        navigate('/login');
+        setMessage("Registration Successful");
+        setUsername("");
+        setPassword("");
+        setEmail("");
+        navigate("/login");
       } else {
         setError(`Failed to register. Status: ${response.status}`);
       }
     } catch (error) {
-      console.error('Error registering:', error.message);
-      setError('Registration failed. Please check your inputs.');
+      console.error("Error registering:", error.message);
+      setError("Registration failed. Please check your inputs.");
     }
   };
 
@@ -60,8 +63,8 @@ const RegisterPage = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <button onClick={handleRegister}>Register</button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        {message && <p style={{ color: 'green' }}>{message}</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {message && <p style={{ color: "green" }}>{message}</p>}
       </div>
     </div>
   );
